0f6c96e268762d4e7166c598c9cdd4ada2ecc151
ZombieNDD <nguyen.d.dung00@gmail.com>
2022-03-23 09:18:45 +0700

    Add project files.

---
+++ Lab04_NguyenDucDung/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using Lab04_NguyenDucDung.Models;
+using Lab04_NguyenDucDung.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab04_NguyenDucDung.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Lab04_NguyenDucDung/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab04_NguyenDucDung.Startup))]
+namespace Lab04_NguyenDucDung
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
